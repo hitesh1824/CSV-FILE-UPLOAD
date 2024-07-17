f44bf4439930d7f8baf63bf4bbe6c94bbe8f762f
unknown <hiteshpathak100150@gmail.com>
2024-07-17 12:43:52 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+
+exports.connectMonggose =()=>{
+  mongoose.connect(process.env.DATABASE_URL,
+  {
+      useNewUrlParser: true
+  })
+  .then((e)=>console.log("Connected to Mongodb =>> CSV Upload"))
+  .catch((e)=>console.log("Not Connect Mongodb"))
+}
+
+
